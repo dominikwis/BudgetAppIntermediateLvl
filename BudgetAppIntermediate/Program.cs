cad37c99fc91eb68f4199815e012801969017ccf
Dominik Wiśniewski <dominikwsw@gmail.com>
2023-09-16 05:57:44 +0200

    Done testing user interface

--- BudgetAppIntermediate/Program.cs
+++ BudgetAppIntermediate/Program.cs
@@ -4,72 +4,75 @@ using BudgetAppIntermediate.Repositories;
 using System.Globalization;
 using System.Xml.Linq;
 
-const string dateFormat = "yyyy-MM-dd";
-DateTime convertedDate;
 string exit = null;
 var billsRepository = new SqlRepository<BillBase>(new BudgetAppIntermediateContext());
-
-AddOneTimeBills(billsRepository);
-AddRegularBills(billsRepository);
-WriteAllToConsole(billsRepository);
 
 Console.WriteLine("***********************************");
 Console.WriteLine("Welcome to budget app!");
 Console.WriteLine("***********************************");
 
-Console.WriteLine("Write 'o' if you want to add One Time Bills or 'r' if Regular Bills. You can change it whenever you want.");
+Console.WriteLine("Write 'o' if you want to add One Time Bills or 'r' if Regular Bills.");
 string option = Console.ReadLine();
 
 while (exit == null)
 {
     if (option == "o")
     {
-        Console.WriteLine("Name: ");
-        string name = Console.ReadLine();
-
-        Console.WriteLine("Amount: ");
-        string amount = Console.ReadLine();
-
-        if (!decimal.TryParse(amount, out decimal convertedAmount))
-        {
-            Console.WriteLine("Invalid number format");
-        }
-
-        Console.WriteLine("Date (with correct format 'yyyy-MM-dd'): ");
-        string date = Console.ReadLine();
-
-        if (!DateTime.TryParseExact(date, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out convertedDate))
-        {
-            Console.WriteLine("Invalid date format. Please use 'yyyy-MM-dd' for example: 2023-03-30.");
-        }
-
-        // Trzeba stworzyć nowy obiekt z tymi zebranymi danymi i przypisać go do jakiejś zmiennej, następnie zastosować bezpośrednio metode billRepository i użyć metody Add();
+        AddOneTimeBills(billsRepository);
 
         Console.WriteLine("You added everthing correct!");
-        Console.WriteLine("Write 'o' if you want to add One Time Bills.");
         Console.WriteLine("Write 'r' if you want to add Regular Bills");
         Console.WriteLine("write 'x' if you want to summarize your budget and exit the app");
+        option = Console.ReadLine();
 
         if (option == "x")
         {
-            exit = "x";
+            WriteAllToConsole(billsRepository);
+            break;
         }
-        else
+        else if (option == "r")
         {
-            option = Console.ReadLine();
+            AddRegularBills(billsRepository);
+
+            Console.WriteLine("You added everthing correct!");
+            Console.WriteLine("Your summarize result:");
+
+            WriteAllToConsole(billsRepository);
+            break;
         }
     }
     else if (option == "r")
     {
-        Console.WriteLine("you already clicked 'r' option");
+        AddRegularBills(billsRepository);
+
+        Console.WriteLine("You added everthing correct!");
+        Console.WriteLine("Write 'o' if you want to add One Time Bills");
+        Console.WriteLine("write 'x' if you want to summarize your budget and exit the app");
+        option = Console.ReadLine();
+
+        if (option == "x")
+        {
+            WriteAllToConsole(billsRepository);
+            break;
+        }
+        else if (option == "o")
+        {
+            AddOneTimeBills(billsRepository);
+
+            Console.WriteLine("You added everthing correct!");
+            Console.WriteLine("Your summarize result:");
+
+            WriteAllToConsole(billsRepository);
+            break;
+        }
     }
 }
 
 static void AddOneTimeBills(IWriteRepository<OneTimeBills> addOneTimeBills)
 {
-    addOneTimeBills.Add(new OneTimeBills { Name = "Shopping", Amount = 30, Date = "25.08.2023" });
-    addOneTimeBills.Add(new OneTimeBills { Name = "Restaurant", Amount = 120, Date = "07.08.2023" });
-    addOneTimeBills.Add(new OneTimeBills { Name = "Computer", Amount = 3000, Date = "02.08.2023" });
+    addOneTimeBills.Add(new OneTimeBills { Name = "Shopping", Amount = 30, Date = "2023-08-30" });
+    addOneTimeBills.Add(new OneTimeBills { Name = "Restaurant", Amount = 120, Date = "2023-08-07" });
+    addOneTimeBills.Add(new OneTimeBills { Name = "Computer", Amount = 3000, Date = "2023-08-02" });
     addOneTimeBills.Save();
 }
 
